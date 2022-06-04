b62789d7efde4010e455f4f39ed2e3fddbef8b85
wxq <1538106781@qq.com>
2022-06-04 21:20:22 +0800

    第一个版本提交

---
+++ src/main/java/com/wang/gittest.java
@@ -0,0 +1,10 @@
+package com.wang;
+
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
